0e73cb2c0bedb35b9c6966de63eb4075e713397c
Craig McFarlane <craig@delaneymorgan.com.au>
2026-07-26 10:41:36 +1000

    - explicitly including required headers

--- dbc_def.h
+++ dbc_def.h
@@ -104,9 +104,11 @@
 
 
 #include <assert.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <syslog.h>
+#include <unistd.h>
 
 // =====================================================================
 
